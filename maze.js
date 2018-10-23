ceb2ee68092b52624d4898dee0986682e2fbd681
Shiloh Waite Dacosta <shilohwaite@gmail.com>
2018-10-23 08:44:40 -0500

    Cleaning up white spaces and checking commits

--- maze.js
+++ maze.js
@@ -6,8 +6,26 @@ window.onload= function() {
     let win = false;
     for(let index =0; index< gwall.length-1; index++){
         gwall[index].addEventListener("mouseover", function(){
+            for(index =0; index< gwall.length-1; index++){
                 gwall[index].setAttribute('class','boundary youlose');
-                hwall =true;
+            }
+             hwall = true;
+             stats.innerHTML = "You Lose!";
         });
     }
+    let sGame =document.getElementById('start');
+    sGame.addEventListener("mouseover", function(){
+        let eGame = document.getElementById("end");
+        eGame.addEventListener("mouseover", function(){
+            if (hwall === false){
+                stats.innerHTML = "You Win!";
+                }
+            });
+        });
+    sGame.addEventListener("click", function(){
+        for (let index = 0; index<gwall.length-1; index ++){
+            gwall[index].setAttribute("class","boundary");
+            stats.innerHTML = "Move your mouse over the 'S' to begin";
+        } hwall=false;  
+    });
 }
