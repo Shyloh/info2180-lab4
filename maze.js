209345c7e5b6e1615226dd3b8bc31d93f43cad1e
Shiloh Waite Dacosta <shilohwaite@gmail.com>
2018-10-23 08:31:06 -0500

    Exercise 1

--- maze.js
+++ maze.js
@@ -6,34 +6,8 @@ window.onload= function() {
     let win = false;
     for(let index =0; index< gwall.length-1; index++){
         gwall[index].addEventListener("mouseover", function(){
-            for(index =0; index< gwall.length-1; index++){
                 gwall[index].setAttribute('class','boundary youlose');
-            }
-             hwall = true;
-             stats.innerHTML = "You Lose!";
+                hwall =true;
         });
-
     }
-
-
-    let sGame =document.getElementById('start');
-    sGame.addEventListener("mouseover", function(){
-        let eGame = document.getElementById("end");
-        eGame.addEventListener("mouseover", function(){
-            if (hwall === false){
-                stats.innerHTML = "You Win!";
-                }
-            });
-        });
-
-    sGame.addEventListener("click", function(){
-        for (let index = 0; index<gwall.length-1; index ++){
-            gwall[index].setAttribute("class","boundary");
-            stats.innerHTML = "Move your mouse over the 'S' to begin";
-        }
-        hwall=false;
-        
-    });
-    
-    
 }
